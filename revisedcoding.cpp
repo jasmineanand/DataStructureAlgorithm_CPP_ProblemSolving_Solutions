110ba607211bb5cfb9f4253f901816b4ed1bf50f
Jasmine Anand <jasmineanand2004@gmail.com>
2022-11-20 20:54:44 +0530

    feat: revised code

---
+++ revisedcoding.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;
+    int n;
+    cin >> n;
+    int a[100];
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int k;
+    cin >> k;
+    i = 0;
+    while (i < n)
+    {
+        if (a[i] == k)
+        {
+            cout << "yes";
+            break;
+        }
+        i++;
+    }
+    if (i == n)
+    {
+        cout << "false";
+    }
+}

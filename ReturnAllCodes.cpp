d561074c2bba1a2f8048a53c017334cb29521b4f
Jasmine Anand <jasmineanand2004@gmail.com>
2022-11-22 22:36:35 +0530

    feat: return codes

--- ReturnAllCodes.cpp
+++ ReturnAllCodes.cpp
@@ -1,17 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-string keypad[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-void code(string a)
+void printAllPossibleCodes(string input, string output)
 {
-    char ch = a[0];
-    string b = keypad[ch - '0'];
-    for (int i = 0; i < b.length(); i++)
+    if (input.empty())
     {
+        cout << output << endl;
+        return;
+    }
+    char c1 = input[0] - '0' + 'a' - 1;
+    int b = stoi(input.substr(0, 2));
+    char c2 = b - 1 + 'a';
+    printAllPossibleCodes(input.substr(1), output + c1);
+    if (b >= 10 && b <= 26)
+    {
+        printAllPossibleCodes(input.substr(2), output + c2);
     }
 }
 int main()
 {
-    string a;
-    cin >> a;
-    code(a);
+    string s;
+    cin >> s;
+    printAllPossibleCodes(s, " ");
+    return 0;
 }

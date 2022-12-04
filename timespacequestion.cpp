398f6980e6f94f0f16b6c1d955957685f78ea8b1
Jasmine Anand <jasmineanand2004@gmail.com>
2022-12-04 23:35:04 +0530

    feat: time and space

---
+++ timespacequestion.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, k;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < k; j++)
+        {
+            cout << (i + j) << endl;
+        }
+    }
+}
